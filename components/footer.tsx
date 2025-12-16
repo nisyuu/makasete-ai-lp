b2a339929bf524e9be0dbc7b008b6b1fd078bb53
にしゅう <37263474+nisyuu@users.noreply.github.com>
2025-12-16 14:47:15 +0900

    Updated footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
     company: {
       title: '開発者情報',
       links: [
-        { label: '会社概要', href: 'https://x.com/nishilyuu' },
+        { label: 'X', href: 'https://x.com/nishilyuu' },
       ],
     },
     // legal: {
@@ -73,7 +73,7 @@ export function Footer() {
             © 2025 Makasete AI. All rights reserved.
           </p>
           <div className="flex space-x-6">
-            <a
+            {/* <a
               href="#"
               className="text-muted-foreground hover:text-primary transition-colors"
               aria-label="Twitter"
@@ -111,7 +111,7 @@ export function Footer() {
               >
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
